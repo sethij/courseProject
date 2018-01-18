ad4531500a42e23bbdb3193ce2817eb37e789a1b
Jagriti Sethi <sethi.jagriti@gmail.com>
2018-01-18 17:54:12 +0530

    Recipe and Ingredient model added and structure mofified

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+recipes: Recipe[] = [
+	new Recipe('Test Recipe','This is test','http://del.h-cdn.co/assets/16/36/980x490/landscape-1473392204-chorizo-bolognese-pastal1.jpg'),
+	new Recipe('Test Recipe','This is test','http://del.h-cdn.co/assets/16/36/980x490/landscape-1473392204-chorizo-bolognese-pastal1.jpg')
+];
   constructor() { }
 
   ngOnInit() {
